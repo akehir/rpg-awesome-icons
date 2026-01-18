53201bf6640dd4533dedb81501674433b13b7900
Raphael Ochsenbein <raphael@akehir.com>
2026-01-18 09:55:13 +0100

    Cleanup

--- projects/example-app/src/test.ts
+++ projects/example-app/src/test.ts
@@ -2,14 +2,15 @@
 
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+  BrowserTestingModule,
+  platformBrowserTesting
+} from '@angular/platform-browser/testing';
+
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
+  BrowserTestingModule,
+  platformBrowserTesting(), {
     teardown: { destroyAfterEach: false }
-}
+  }
 );
